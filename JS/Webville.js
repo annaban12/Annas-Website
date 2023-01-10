32fdefd936227a24fda4f19c0235106fe7a58711
annaban12 <annalapidus2@gmail.com>
2023-01-10 08:45:37 -0500

    u

--- JS/Webville.js
+++ JS/Webville.js
@@ -1,4 +1,30 @@
+window.onload = init;
+function init() {
+  var button = document.getElementById("addButton");
+  button.onclick = handleButtonClick;
 
+var loc = window.location.href;
+var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                      loc + '?profile=css3';
+document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
+window.onload = init;
+function handleButtonClick() {
+  var textInput = document.getElementById("songTextInput");
+  var songName = textInput.value;
+  if (songName == "") {
+    alert("Please enter a song");
+  }
+  if (songName == "") {
+    var li=document.createElement("")
+  }
+  var li=document.createElement("li");
+  li.innerHTML = songName;
+  var ul = document.getElementById("playlist");
+  ul.appendChild(li);
+}
 
 
 
